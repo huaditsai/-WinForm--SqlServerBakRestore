4d311ffc5386517fc03b6e99bcab0517bd9b1f0c
huadi73 <huadi73@hotmail.com>
2014-04-16 10:14:02 +0800

    1

---
+++ SqlServerBakRestore/SqlServerBakRestore/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace SqlServerBakRestore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.txtDb_Log_Path = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDb_Data_Path = new System.Windows.Forms.TextBox();
+            this.btnTestConnect = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.labTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(14, 24);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(442, 22);
+            this.txtConnectionString.TabIndex = 0;
+            this.txtConnectionString.Text = "Data Source=THOMAS-SQL-3\\SQLSERVER2;Trusted_Connection=true";
+            // 
+            // txtDb_Log_Path
+            // 
+            this.txtDb_Log_Path.Location = new System.Drawing.Point(16, 124);
+            this.txtDb_Log_Path.Name = "txtDb_Log_Path";
+            this.txtDb_Log_Path.Size = new System.Drawing.Size(440, 22);
+            this.txtDb_Log_Path.TabIndex = 2;
+            this.txtDb_Log_Path.Text = "D:\\MSSQL2\\DATA";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(16, 173);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(440, 22);
+            this.txtPath.TabIndex = 3;
+            this.txtPath.Text = "D:\\DB";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Connection String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Db Data Path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Db Log Path";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = ".BAK File Path";
+            // 
+            // txtDb_Data_Path
+            // 
+            this.txtDb_Data_Path.Location = new System.Drawing.Point(14, 74);
+            this.txtDb_Data_Path.Name = "txtDb_Data_Path";
+            this.txtDb_Data_Path.Size = new System.Drawing.Size(442, 22);
+            this.txtDb_Data_Path.TabIndex = 1;
+            this.txtDb_Data_Path.Text = "D:\\MSSQL2\\DATA";
+            // 
+            // btnTestConnect
+            // 
+            this.btnTestConnect.Location = new System.Drawing.Point(16, 201);
+            this.btnTestConnect.Name = "btnTestConnect";
+            this.btnTestConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnTestConnect.TabIndex = 8;
+            this.btnTestConnect.Text = "Test Connect";
+            this.btnTestConnect.UseVisualStyleBackColor = true;
+            this.btnTestConnect.Click += new System.EventHandler(this.btnTestConnect_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(109, 201);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(75, 23);
+            this.btnRestore.TabIndex = 9;
+            this.btnRestore.Text = "Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 421);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(364, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(14, 230);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(442, 185);
+            this.txtInfo.TabIndex = 11;
+            // 
+            // labTime
+            // 
+            this.labTime.AutoSize = true;
+            this.labTime.Font = new System.Drawing.Font("新細明體", 12F);
+            this.labTime.Location = new System.Drawing.Point(389, 425);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(64, 16);
+            this.labTime.TabIndex = 12;
+            this.labTime.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 456);
+            this.Controls.Add(this.labTime);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnTestConnect);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtDb_Log_Path);
+            this.Controls.Add(this.txtDb_Data_Path);
+            this.Controls.Add(this.txtConnectionString);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.TextBox txtDb_Log_Path;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDb_Data_Path;
+        private System.Windows.Forms.Button btnTestConnect;
+        private System.Windows.Forms.Button btnRestore;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SqlServerBakRestore/SqlServerBakRestore/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlServerBakRestore
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString;
+        static string db_Data_Path;
+        static string db_Log_Path;
+        string path;
+
+        int count = 0;//總數量
+        int time = 0;
+        int h = 0, m = 0, s = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            timer1.Interval = 1000; // 設定每秒觸發一次
+        }
+
+        private void init()
+        {
+            connectionString = txtConnectionString.Text;
+            db_Data_Path = txtDb_Data_Path.Text;
+            db_Log_Path = txtDb_Log_Path.Text;
+            path = txtPath.Text;
+            count = 0;
+        }
+
+        private void GetFolder(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path);/*目錄(含路徑)的陣列*/
+            string folderName;
+            string[] fileNames;
+            int i = 0;
+
+            //System.Collections.ArrayList dirlist = new System.Collections.ArrayList();/*用來儲存只有目錄名的集合*/
+
+            foreach (string item in dirs)
+            {
+                folderName = Path.GetFileNameWithoutExtension(item);//走訪每個元素只取得目錄名稱(不含路徑)並加入dirlist集合中
+                fileNames = Directory.GetFileSystemEntries(item.ToString());//取得資料夾下的檔案名稱(含路徑,附檔名)
+
+                foreach (var fileName in fileNames)
+                    //if (fileName.Contains("20130312"))
+                    count++;
+            }
+
+            backgroundWorker1.ReportProgress(0, "共有 " + count);
+
+            foreach (string item in dirs)
+            {
+                folderName = Path.GetFileNameWithoutExtension(item);//走訪每個元素只取得目錄名稱(不含路徑)並加入dirlist集合中
+                fileNames = Directory.GetFileSystemEntries(item.ToString());//取得資料夾下的檔案名稱(含路徑,附檔名)
+
+                foreach (var fileName in fileNames)
+                {
+                    //if (fileName.Contains("20130312"))
+                    //{
+                    //Console.WriteLine(fileName);
+                    backgroundWorker1.ReportProgress(i, fileName);
+                    RestoreDataBase(folderName, fileName);//連接SQL開始還原                        
+                    i++;
+                    //}
+                }
+            }
+            //Console.WriteLine(count);
+        }
+
+        private void testConnection()
+        {
+            try
+            {
+                SqlConnection objSaConn = new SqlConnection(connectionString);
+                objSaConn.Open();
+                MessageBox.Show("connect success " + count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void RestoreDataBase(string DatabaseName, string BakFileName)
+        {
+            string DataFile = "", DataFilePath = "", LogFile = "", LogFilePath = "";
+
+            SqlConnection objSaConn = new SqlConnection(connectionString);
+            SqlCommand cmd;
+            objSaConn.Open();
+
+            try
+            {
+                cmd = new SqlCommand("RESTORE FILELISTONLY FROM DISK = '" + BakFileName + "'", objSaConn);
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    if (rd["Type"].ToString() == "D")
+                    {
+                        DataFile = rd["LogicalName"].ToString();
+                        DataFilePath = rd["PhysicalName"].ToString();
+                    }
+                    else if (rd["Type"].ToString() == "L")
+                    {
+                        LogFile = rd["LogicalName"].ToString();
+                        LogFilePath = rd["PhysicalName"].ToString();
+                    }
+                }
+                rd.Close();
+
+                cmd = new SqlCommand("Use Master", objSaConn);
+                cmd.ExecuteNonQuery();
+
+                //MessageBox.Show("DataFilePath : " + DataFilePath + "\r\n" +
+                //    "LogFilePath : " + LogFilePath + "\r\n");
+
+                String sqlString;
+
+                sqlString = Environment.NewLine + string.Format(
+                    "RESTORE DATABASE [{0}] " +
+                    "FROM DISK = '{1}' " +
+                    "WITH " +
+                    "MOVE '{2}' to '{3}', " +
+                    "MOVE '{4}' to '{5}', " +
+                    "NORECOVERY, Replace",
+                    DatabaseName, BakFileName,
+                    DataFile, db_Data_Path + "\\" + DataFilePath.Substring(DataFilePath.LastIndexOf("\\")),
+                    LogFile, db_Log_Path + "\\" + LogFilePath.Substring(LogFilePath.LastIndexOf("\\")));
+
+                cmd = new SqlCommand(sqlString, objSaConn);
+                cmd.CommandTimeout = 0; //預設30s
+                cmd.ExecuteNonQuery();
+
+                //MessageBox.Show("123");
+
+                sqlString = Environment.NewLine + string.Format(
+                    "RESTORE DATABASE [{0}] " +
+                    "FROM DISK = '{1}' ",
+                    DatabaseName, BakFileName);
+
+                cmd = new SqlCommand(sqlString, objSaConn);
+                cmd.CommandTimeout = 0;
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter sw = System.IO.File.AppendText("Error.txt"))
+                {
+                    sw.WriteLine(DatabaseName + " : " + ex.Message + "\r\n");
+                }
+            }
+        }
+
+
+        //        BACKUP DATABASE [SyncSamplesDb_SqlPeer1] 
+        //TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQL\Backup\SyncSamplesDb_SqlPeer1.bak' 
+        //WITH NOFORMAT, NOINIT,  NAME = N'SyncSamplesDb_SqlPeer1-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+
+
+        private void btnTestConnect_Click(object sender, EventArgs e)
+        {
+            init();
+
+            string[] dirs = Directory.GetDirectories(path);/*目錄(含路徑)的陣列*/
+            string folderName;
+            string[] fileNames;
+
+            foreach (string item in dirs)
+            {
+                folderName = Path.GetFileNameWithoutExtension(item);//走訪每個元素只取得目錄名稱(不含路徑)並加入dirlist集合中
+                fileNames = Directory.GetFileSystemEntries(item.ToString());//取得資料夾下的檔案名稱(含路徑,附檔名)
+
+                foreach (var fileName in fileNames)
+                    count++;
+            }
+
+            testConnection();
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true; // 啟動 Timer
+            timer1.Start();
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            init();
+            GetFolder(path);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            try
+            {
+                progressBar1.Maximum = count - 1; //用於progressBar
+                progressBar1.Value = e.ProgressPercentage;
+
+                txtInfo.Text += "Restoring " + e.UserState + "\r\n";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            timer1.Stop();
+            txtInfo.Text += "Complete";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+
+            h = time / 60 / 60;
+            m = time / 60 % 60;
+            s = time % 60 % 60;
+
+            labTime.Text = h.ToString("D2") + ":" + m.ToString("D2") + ":" + s.ToString("D2");
+        }
+    }
+}
